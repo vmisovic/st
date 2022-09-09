1fcf63f8940152f4edd9c1dc0194cef569288289
vmisovic <vladimir.misovic03@gmail.com>
2022-09-09 12:06:18 +0200

    new font: 'JetBrainsMono Nerd Font', tag 2. icon, removed config.h and .gitignore

--- config.def.h
+++ config.def.h
@@ -6,11 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
-static char *font = "JetBrainsMono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:style=Medium:pixelsize=15:antialias=true:autohint=true";
 /* Spare fonts */
-static char *font2[] = {"SpaceMono Nerd Font Mono:style=Regular"
-/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+static char *font2[] = {
+//	"SpaceMono Nerd Font Mono:style=Regular",
+//	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
+//	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
 };
 static int borderpx = 4;
 
