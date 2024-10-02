f6c440a9175bc4fbe671bfede4362c26d3b0f216
vmisovic <vladimir.misovic03@gmail.com>
2024-10-02 22:17:08 +0200

    font size

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
-static char *font = "JetBrainsMono Nerd Font:style=Medium:pixelsize=15:antialias=true:autohint=true";
+static char *font = "JetBrainsMono NFM:style=Medium:pixelsize=22:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 //	"SpaceMono Nerd Font Mono:style=Regular",
